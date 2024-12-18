a2fe38c7c6b81932b12c2c29b771b70ab0c6e17e
daishadavis <daishadavis84@gmail.com>
2024-12-18 17:03:41 -0500

    Changes

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
 import Login from './pages/LoginIn';
+
 const App = () => {
   const [selectedCard, setSelectedCard] = useState(null)
   const handleCardClick = (cardTitle) => {
@@ -10,14 +10,7 @@ const App = () => {
     <div>
       <Login />
     </div>
-=======
-import User from './User'
-
-const App = () => {
-  return (
-    <div>Welcome to our React App! </div>
->>>>>>> 6914adac5c126aa6848c06154fecb65f5b93430e
   )
-};
+}
 
 export default App;
